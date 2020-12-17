617f70f5423021efa4521b8fd7efa2872477f848
Olena Tsemko <elena.hlihan@gmail.com>
2020-12-17 17:31:39 +0200

    switch color

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import './styles.css';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+const colorSwitch = {
+  intervalId: null,
+  isActive: false,
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      refs.body.style.backgroundColor =
+        colors[randomIntegerFromInterval(0, colors.length)];
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+
+    this.isActive = false;
+    this.intervalId = null;
+  },
+};
+
+refs.btnStart.addEventListener('click', colorSwitch.start.bind(colorSwitch));
+refs.btnStop.addEventListener('click', colorSwitch.stop.bind(colorSwitch));
